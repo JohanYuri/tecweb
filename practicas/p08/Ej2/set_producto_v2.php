709c46eba39f45421b95f9540018215292604502
JohanYuri <johanyuri24@gmail.com>
2025-02-22 20:24:47 -0600

    Se corrigió ejercicio 2

--- practicas/p08/Ej2/set_producto_v2.php
+++ practicas/p08/Ej2/set_producto_v2.php
@@ -6,6 +6,7 @@ $precio   = $_POST['precio'];
 $detalles = $_POST['detalles'];
 $unidades = $_POST['unidades'];
 $imagen   = 'img/imagen.png';
+$eliminado = $_POST['eliminado'];
 
 /** SE CREA EL OBJETO DE CONEXION */
 @$link = new mysqli('localhost', 'root', '1234', 'marketzone');	
@@ -17,15 +18,36 @@ if ($link->connect_errno)
     /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 }
 
+// Validar que nombre, modelo y marca no existan en la base de datos
+$sql = "SELECT * FROM productos WHERE nombre = '{$_POST["nombre"]}' AND modelo = '{$_POST["modelo"]}' AND marca = '{$_POST["marca"]}'";
+$result = $link->query($sql);
+if($result->num_rows > 0)
+{
+    echo '<h1>Error</h1>
+            <p>El producto ya existe en la base de datos</p>';
+    $link->close();
+    return;
+}
+
 /** Crear una tabla que no devuelve un conjunto de resultados */
 $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', '{$eliminado}')";
 if ( $link->query($sql) ) 
 {
-    echo 'Producto insertado con ID: '.$link->insert_id;
+    echo '<h1>Producto</h1>
+    <h2>Datos insertados:</h2>
+    <span>ID: </span>'.$link->insert_id.'
+    <br><span>Nombre: </span>'.$_POST["nombre"].'
+    <br><span>Marca: </span>'.$_POST["marca"].'
+    <br><span>Modelo: </span>'.$_POST["modelo"].'
+    <br><span>Precio: $</span>'.$_POST["precio"].'
+    <br><span>Detalles: </span>'.$_POST["detalles"].'
+    <br><span>Unidades: </span>'.$_POST["unidades"].'
+    <br><span>Imagen: </span>'.$_POST["imagen"].'
+    <br><span>Eliminado: 0 (Falso)</span>';
 }
 else
 {
-	echo 'El Producto no pudo ser insertado =(';
+    echo 'El Producto no pudo ser insertado =(';
 }
 
 $link->close();
